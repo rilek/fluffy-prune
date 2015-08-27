1689956f7723f1dc9b0b53b14a07b764747d2fd7
Rafal <r.ileczko@gmail.com>
2015-08-27 21:40:32 +0200

    Kolorki

--- R/Rlib.js
+++ R/Rlib.js
@@ -47,14 +47,27 @@ var R = {};
 	};
 
 //dodaj event
-function addEvent(obj, evType, fn, useCapture){
-  if (obj.addEventListener){
-    obj.addEventListener(evType, fn, useCapture);
-    return true;
-  } else if (obj.attachEvent){
-    var r = obj.attachEvent("on"+evType, fn);
-    return r;
-  } else {
-    alert("Handler could not be attached");
-  }
+	R.addEvent = function(obj, evType, fn, useCapture){
+	  if (obj.addEventListener){
+	    obj.addEventListener(evType, fn, useCapture);
+	    return true;
+	  } else if (obj.attachEvent){
+	    var r = obj.attachEvent("on"+evType, fn);
+	    return r;
+	  } else {
+	    alert("Handler could not be attached");
+	  }
+	}
+
+//multiple setAttribute
+/*
+	node, {
+	'attr':'value'
+	'attr2':'value2'
 }
+*/
+	R.setAttributes = function(el, attrs) {
+	  for(var key in attrs) {
+	    el.setAttribute(key, attrs[key]);
+	  }
+	};
--- canvas/scripts.js
+++ canvas/scripts.js
@@ -1,28 +1,126 @@
 (function() {
+	//var
 	var c = document.getElementById("myCanv");
 	var ctx = c.getContext("2d");
 
-	var draw = document.getElementById("opt");
-	var clear = document.getElementById("clear")
-
+	var form = document.getElementById("opt");
+	var clear = document.getElementById("clear");
+	var submit = document.getElementById("draw");
+	var type = document.getElementById("type");
+	var fillFlag;
 	c.width = (window.innerWidth * 60)/100;
 
-	addEvent(window, 'resize', function(e) {
-		c.width = (window.innerWidth * 60)/100;
+
+	//Select options 
+	R.addEvent(type, 'change', function() {
+		var fieldset = document.getElementById("userOpt");
+		while(fieldset.firstChild) {
+			fieldset.removeChild(fieldset.firstChild);
+		}
+		switch(type.value) {
+			//Opcje dla Prostokątów
+			case 'rect':
+				var wLabel = document.createElement("label");
+				var hLabel = document.createElement("label");
+				var wInput = document.createElement("input");
+				var hInput = document.createElement("input");
+				wLabel.setAttribute("for", "width");
+				hLabel.setAttribute("for", "height");
+				R.setAttributes(wInput, {
+					'name' : 'width',
+					'id' : 'width',
+					'type' : 'number',
+					'placeholder' : '0',
+					'required' : ''
+				});
+				R.setAttributes(hInput, {
+					'name' : 'height',
+					'id' : 'height',
+					'type' : 'number',
+					'placeholder' : '0',
+					'required' : ''
+				});
+				wText = document.createTextNode("Szerokość ");
+				hText = document.createTextNode("Wysokość ");
+				wLabel.appendChild(wText);
+				hLabel.appendChild(hText);
+				wLabel.appendChild(wInput);
+				hLabel.appendChild(hInput);
+				fieldset.appendChild(wLabel);
+				fieldset.appendChild(hLabel);
+				break;
+			//Opcje dla Elips
+			case 'circle':
+				var rLabel = document.createElement("label");
+				var rInput = document.createElement("input");
+				var rText = document.createTextNode("Promień");
+				R.setAttributes(rInput, {
+					'name' : 'radius',
+					'id' : 'radius',
+					'type' : 'number',
+					'placeholder' : '0'
+				});
+				rLabel.appendChild(rText);
+				rLabel.appendChild(rInput);
+				fieldset.appendChild(rLabel);
+				break;
+			case 'arc':
+				break;
+			default:
+				break;
+		}
+	});
+
+	//Resize canvas
+	R.addEvent(window, "resize", function(e) {
+		if(window.innerWidth > 800){
+
+			c.width = (window.innerWidth * 60)/100;
+		} else {
+			document.getElementById("container").style.display = 'block';
+		}
 	}, false);
 
-	addEvent(draw, 'submit', function(e) {
+	//Draw element
+	R.addEvent(form, "submit", function(e) {
+
 		var x = document.getElementById("startX").value;
 		var y = document.getElementById("startY").value;
-		var width = document.getElementById("width").value;
-		var height = document.getElementById("height").value;
-		ctx.fillStyle = '#000';
-		ctx.fillRect(x,y,width,height);
+
+		ctx.fillStyle = '#' + document.getElementById("colorPicker").value;
+		ctx.strokeStyle = '#000';
+		switch(type.value) {
+			case 'rect':
+				ctx.beginPath();
+				var width = document.getElementById("width").value;
+				var height = document.getElementById("height").value;
+				ctx.fillRect(x,y,width,height);
+				break;
+			case 'circle':
+				var radius = document.getElementById("radius").value;
+				ctx.beginPath();
+				ctx.arc(x,y,radius,0,2*Math.PI, false);
+				ctx.fill();
+				break;
+			case 'arc':
+				break;
+			default:
+				break;
+		}
 		e.preventDefault();
 	}, false);
 
-	addEvent(clear, 'click', function(){
+	//Clear element
+	R.addEvent(clear, "click", function(){
 		ctx.clearRect(0,0,c.width, c.height);
 	})
 
+
+	//Symulating change on first select option
+	var ev = document.createEvent("HTMLEvents");
+	ev.initEvent("change", true, true);
+	type.dispatchEvent(ev);
+
+
+	
 })();
